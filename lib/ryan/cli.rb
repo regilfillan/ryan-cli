c43399fd3cb946f850925309587a82d654a4801f
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-14 23:44:50 +0000

    Working through the Command Line Interface

--- lib/ryan/cli.rb
+++ lib/ryan/cli.rb
@@ -1,8 +1,28 @@
-require "ryan/cli/version"
+require_relative "../lib/scraper.rb"
+require_relative "../lib/locations.rb"
+require "nokogiri"
+require "colorize"
 
-module Ryan
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
+class CommandLineInterface
+  BASE_PATH = "bpl.bibliocommons.com/locations/list/"
+  
+  def run 
+    make_locations
+    add_attributes_to_locations
+    display_locations
   end
+  
+  def make_locations
+    locations_array = Scraper.scrape_directory_page(BASE_PATH + "index.html")
+    Location.create_from_collection(locations_array)
+  end
+  
+  def add_attributes_to_locations
+    
+  end
+  
+  def display_locations
+    
+  end
+  
 end
