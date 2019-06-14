c4888712ba0217ceeef6c56f2a72b387ad1a42df
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-14 22:51:31 +0000

    Editing the Scraper file, trying various data to scrape

--- lib/ryan/scraper.rb
+++ lib/ryan/scraper.rb
@@ -4,8 +4,16 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-   
+  def self.scrape_directory_page(directory_url)
+   directory_page= Nokogiri::HTML(open(directory_url))
+   libraries = []
+   directory_page.css("div.locations-list").each do |library|
+     library.css(".location-information-row a").each do |location|
+       location_overview_link = "#{location.attr('href')}"
+       location_address = location.css(".location-address").text
+       location_phone_number = location.css(".location-phone").text 
+       location_email = location.css(".location_email")
+       
   end
 
   def self.scrape_location_page(profile_slug)
