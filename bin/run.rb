f317a2f3bf23ae4bdc931be62d8674a990eee730
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-15 18:41:54 +0000

    working on bin file

---
+++ bin/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# require_relative "../lib/ryan/cli.rb"
+require_relative "../lib/ryan/scraper.rb"
+
+
+Scraper.scrape_directory_page("https://bpl.bibliocommons.com/locations/list/")
--- lib/ryan/scraper.rb
+++ lib/ryan/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
    directory_page= Nokogiri::HTML(open(directory_url))
    libraries = []
    directory_page.css("div.locations-list").each do |library|
+     binding.pry
      library.css(".location-information-row a").each do |location|
        location_overview_link = "#{location.attr('href')}"
        location_address = location.css(".location-address").text
