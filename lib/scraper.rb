6736a606fea5c0508ea9baefb87e083f3e2025be
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-16 02:31:26 +0000

    beefing up a framework for my scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,34 @@ require 'pry'
 class Scraper
 
   def get_page
-    doc = Nokogiri::HTML(open("https://www.podcastinsights.com/top-us-podcasts/"))
+    Nokogiri::HTML(open("https://www.podcastinsights.com/top-us-podcasts/"))
   binding.pry
   end
   
-  # def get_podcasts
-  #   self.get_page.css(".class").css("p strong")
-  # end
+  def get_podcasts
+    self.get_page.css(“yyy”)
+  end
+ 
+  def make_podcasts
+    self.get_podcasts.each do |pod|
+      podcast = Podcast.new
+      podcast.title = yyy.css(“xxxxx").text
+      podcast.network_or_artist = yyy.css(“qqq”).text
+      podcast.where_to_listen = yyy.css(“zzz”).text
+    end
+  end
+ 
+  def print_podcasts
+    self.make_podcasts
+    Podcast.all.each do |pod|
+      if podcast.title
+        puts “Title: #{podcast.title}”
+        puts ”  Network/Artist: #{podcast.network_or_artist}”
+        puts ”  Where to Listen: #{podcast.where_to_listen}”
+      end
+    end
+  end
+ 
 end
  
-Scraper.new.get_page
+Scraper.new.print_podcasts
