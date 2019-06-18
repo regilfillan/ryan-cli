bef9ea84987f8e7ecada3726e17acec36bab5f19
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-18 03:39:39 +0000

    still working out the best css selectors for lis

--- lib/podbay_scraper.rb
+++ lib/podbay_scraper.rb
@@ -9,8 +9,11 @@ class PodbayScraper
   def self.scrape_podcasts
     page= Nokogiri::HTML(open(BASE_URL))
     podcast_lis= page.css('div.row ul.thumbnails li.span3 h4')
-      
-    binding.pry
+    podcast_lis.each do |li|
+      title= li.css(‘div.caption h4’).text
+      artist_or_network= li.css(‘div.caption h6’).text
+      data_url= li.css(’a[href]’).each {|element|element[“href”]}
+      binding.pry
   # titles = table.css('tr td span a')
   end
  
