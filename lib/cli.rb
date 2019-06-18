824f6af203e28d594bb72309e81565b3eef37029
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-18 22:55:35 +0000

    preventing another loss of code due to internet time out

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,6 @@
-class CommandLineInterface
-  
-  def run 
+ class CommandLineInterface
+
+  def run
     welcome
     PodbayScraper.scrape_podcasts
     loop do
@@ -9,38 +9,36 @@ class CommandLineInterface
           return
         else
           self.list_top_podcasts
-          user_input_again= self.choose_podcast
-            #question for micah
-            if user_input_again.is_a? Integer && (0 < user_input <= 19)
+          self.choose_podcast
+          user_input_again = gets.strip.to_i
+            if user_input_again.is_a? Integer && (1 < user_input_again <= 20)
               self.display_podcast_details
             else
               puts "Oops! Please input a number between 1 and 20."
               self.choose_podcast
             end
-            #question for micah
         end
       end
-    #
   end
-  
+
   def welcome
     puts "\n\n"
     puts "Welcome to the Top Twenty Podcast App!"
     puts "\n\n"
   end
-  
+
   def option_menu
     puts "Would you like to see today's Top 20 according to Podbay.fm? (Y/N)"
     puts "\n\n"
-    input = gets.strip.downcase 
+    input = gets.strip.downcase
   end
-  
+
   def list_top_podcasts
     puts "Today's Top 20:\n\n"
     Podcast.all.each_with_index {|pod, i| puts "#{i+1}. #{pod.title}"}
     puts "\n\n"
   end
-  
+
   def choose_podcast
     puts "Select a number from the list to learn more about the corresponding Podcast!"
     index = gets.strip.to_i-1
@@ -48,8 +46,8 @@ class CommandLineInterface
     PodbayScraper.scrape_details(podcast)
     self.display_podcast_details(podcast)
   end
-  
-  def display_podcast_details
+
+  def display_podcast_details(podcast)
     puts "\n\n"
     puts "Copy and paste the following to your URL to see the additional details!"
     puts "Open their website: '#{podcast.open_website}'!"
@@ -57,5 +55,5 @@ class CommandLineInterface
     puts "Check out their existing episodes: '#{podcast.episode_list}'!"
     puts "\n\n"
   end
-  
-end
+
+end 
