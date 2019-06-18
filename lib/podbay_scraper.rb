5142cfde85cabca3f9220100889907671b93a81b
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-18 03:49:23 +0000

    still need to work through data_url scrape

--- lib/podbay_scraper.rb
+++ lib/podbay_scraper.rb
@@ -10,11 +10,11 @@ class PodbayScraper
     page= Nokogiri::HTML(open(BASE_URL))
     podcast_lis= page.css('div.row ul.thumbnails li.span3 h4')
     podcast_lis.each do |li|
-      title= li.css(‘div.caption h4’).text
-      artist_or_network= li.css(‘div.caption h6’).text
-      data_url= li.css(’a[href]’).each {|element|element[“href”]}
+      title= li.css('div.caption h4').text
+      artist_or_network= li.css('div.caption h6').text
+      data_url= li.css('a[href]').each {|element| element['href']}
       binding.pry
-  # titles = table.css('tr td span a')
+    end
   end
  
 end
