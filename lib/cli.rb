3cfccf19beee88a2916a79e0944747fcb2d2d5d9
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-18 18:48:11 +0000

    updates cli with new methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,14 +3,32 @@ class CommandLineInterface
   def run 
     welcome
     PodbayScraper.scrape_podcasts
-    
+    loop do
+      user_input = option_menu
+        if user_input == "exit" || user_input.include?("n")
+          return
+        else
+          self.list_podcasts
+          self.choose_podcast
+        end
   end
   
   def welcome
     puts "Welcome Message"
   end
   
+  def option_menu
+    puts "Would you like to see the Top 20 Podcasts today according to Podbay.fm? (Y/N)"
+    input = gets.strip.downcase 
+    
+  end
+  
   def list_top_podcasts
+    puts "Today's Top 20:\n\n"
+    Podcast.all.each_with_index {|pod, i| puts "#{i+1}. #{pod.title}"}
+  end
+  
+  def choose_podcasts
     
   end
   
